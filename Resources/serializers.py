3a8bd0597e637c02abee704ff1b779dce90e2823
Griffin <omondigriffin4@gmail.com>
2025-01-14 15:21:56 +0300

    makes changes to the serializer.py

--- Resources/serializers.py
+++ Resources/serializers.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
-from .models import Form1Video, Form2Video, Form3Video, Form4Video, Categories
+from .models import Video, Category
 
 
 class HomeSerializer(serializers.Serializer):
     message = serializers.CharField()
-    
-class BaseVideoSerializer(serializers.ModelSerializer):
+
+
+class VideoSerializer(serializers.ModelSerializer):
     video_url = serializers.SerializerMethodField()  # Absolute URL for the video file
     cover_image_url = serializers.SerializerMethodField()  # Absolute URL for the cover image
 
     class Meta:
-        fields = ('id', 'title', 'description', 'video_file', 'cover_image', 'uploaded_at', 'video_url', 'cover_image_url')
+        model = Video
+        fields = ('id', 'title', 'description', 'video_file', 'cover_image', 'category', 'uploaded_at', 'video_url', 'cover_image_url')
 
     def get_video_url(self, obj):
         request = self.context.get('request')
@@ -24,26 +26,16 @@ class BaseVideoSerializer(serializers.ModelSerializer):
             return request.build_absolute_uri(obj.cover_image.url)
         return None
 
-# Define serializers for each form
-class Form1VideoSerializer(BaseVideoSerializer):
-    class Meta(BaseVideoSerializer.Meta):
-        model = Form1Video
-
-class Form2VideoSerializer(BaseVideoSerializer):
-    class Meta(BaseVideoSerializer.Meta):
-        model = Form2Video
-
-class Form3VideoSerializer(BaseVideoSerializer):
-    class Meta(BaseVideoSerializer.Meta):
-        model = Form3Video
-
-class Form4VideoSerializer(BaseVideoSerializer):
-    class Meta(BaseVideoSerializer.Meta):
-        model = Form4Video
 
 class CategoriesSerializer(serializers.ModelSerializer):
-    image_url = serializers.ImageField(required=False)
+    image_url = serializers.SerializerMethodField()  # Absolute URL for the category image
 
     class Meta:
-        model = Categories
-        fields = "__all__"
+        model = Category
+        fields = ('id', 'title', 'image', 'image_url')
+
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if request and obj.image:
+            return request.build_absolute_uri(obj.image.url)
+        return None
