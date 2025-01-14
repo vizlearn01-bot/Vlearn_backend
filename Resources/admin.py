4d3c7edcad3b2a3cafa7f96f9bafaa0d1ed7830a
Griffin <omondigriffin4@gmail.com>
2025-01-14 15:28:20 +0300

    modifies the admin.py file

--- Resources/admin.py
+++ Resources/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import Video, Category
 
-# Register each form-level video model separately
-@admin.register(Video)
-
-# Register the Categories model
-@admin.register(Category)
-class CategoriesAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image')
-    search_fields = ('title',)
+admin.site.register(Video)
+admin.site.register(Category)
--- Resources/urls.py
+++ Resources/urls.py
@@ -1,30 +1,17 @@
 from django.urls import path
 from .views import (
-    Form1VideoUploadView, Form1VideoListView,
-    Form2VideoUploadView, Form2VideoListView,
-    Form3VideoUploadView, Form3VideoListView,
-    Form4VideoUploadView, Form4VideoListView,
-    CategoriesView, Home
+    VideoUploadView,
+    VideoListView,
+    CategoriesView,
+    Home
 )
 
 urlpatterns = [
-
     path('', Home.as_view(), name='home'),
-    # Form 1 Video URLs
-    path('upload/form1/', Form1VideoUploadView.as_view(), name='form1-video-upload'),
-    path('videos/form1/', Form1VideoListView.as_view(), name='form1-video-list'),
 
-    # Form 2 Video URLs
-    path('upload/form2/', Form2VideoUploadView.as_view(), name='form2-video-upload'),
-    path('videos/form2/', Form2VideoListView.as_view(), name='form2-video-list'),
-
-    # Form 3 Video URLs
-    path('upload/form3/', Form3VideoUploadView.as_view(), name='form3-video-upload'),
-    path('videos/form3/', Form3VideoListView.as_view(), name='form3-video-list'),
-
-    # Form 4 Video URLs
-    path('upload/form4/', Form4VideoUploadView.as_view(), name='form4-video-upload'),
-    path('videos/form4/', Form4VideoListView.as_view(), name='form4-video-list'),
+    # Video URLs
+    path('upload/video/', VideoUploadView.as_view(), name='video-upload'),
+    path('videos/', VideoListView.as_view(), name='video-list'),
 
     # Categories
     path('categories/', CategoriesView.as_view(), name='categories'),
