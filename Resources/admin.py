efd906777689708bda06c211a43b5c72e3de228a
Griffin <omondigriffin4@gmail.com>
2025-01-14 15:19:17 +0300

    changes the view.py to reflect the changes made in the model.py

--- Resources/admin.py
+++ Resources/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Video, Categories
+from .models import Video, Category
 
 # Register each form-level video model separately
 @admin.register(Video)
 
 # Register the Categories model
-@admin.register(Categories)
+@admin.register(Category)
 class CategoriesAdmin(admin.ModelAdmin):
     list_display = ('title', 'image')
     search_fields = ('title',)
--- Resources/views.py
+++ Resources/views.py
@@ -2,75 +2,44 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
-from .models import Form1Video, Form2Video, Form3Video, Form4Video, Categories
-from .serializers import Form1VideoSerializer, Form2VideoSerializer, Form3VideoSerializer, Form4VideoSerializer, CategoriesSerializer, HomeSerializer
+from .models import Video, Category
+from .serializers import VideoSerializer, CategoriesSerializer, HomeSerializer
 
 
+# Home view
 class Home(APIView):
     def get(self, request, *args, **kwargs):
         data = {"message": "Welcome to the home page!"}
         serializer = HomeSerializer(data)
         return Response(serializer.data)
-    
-class BaseVideoUploadView(APIView):
+
+
+# Video upload view
+class VideoUploadView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
-    serializer_class = None
-    model_class = None
-
     def post(self, request, *args, **kwargs):
-        video_serializer = self.serializer_class(data=request.data)
+        video_serializer = VideoSerializer(data=request.data)
         if video_serializer.is_valid():
             video_serializer.save()
             return Response(video_serializer.data, status=status.HTTP_201_CREATED)
         return Response(video_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class Form1VideoUploadView(BaseVideoUploadView):
-    serializer_class = Form1VideoSerializer
-    model_class = Form1Video
 
-class Form2VideoUploadView(BaseVideoUploadView):
-    serializer_class = Form2VideoSerializer
-    model_class = Form2Video
-
-class Form3VideoUploadView(BaseVideoUploadView):
-    serializer_class = Form3VideoSerializer
-    model_class = Form3Video
-
-class Form4VideoUploadView(BaseVideoUploadView):
-    serializer_class = Form4VideoSerializer
-    model_class = Form4Video
-
-class BaseVideoListView(APIView):
-    serializer_class = None
-    model_class = None
-
+# Video list view
+class VideoListView(APIView):
     def get(self, request, *args, **kwargs):
-        videos = self.model_class.objects.all()
-        video_serializer = self.serializer_class(videos, many=True, context={'request': request})
+        videos = Video.objects.all()
+        video_serializer = VideoSerializer(videos, many=True, context={'request': request})
         return Response(video_serializer.data)
 
-class Form1VideoListView(BaseVideoListView):
-    serializer_class = Form1VideoSerializer
-    model_class = Form1Video
 
-class Form2VideoListView(BaseVideoListView):
-    serializer_class = Form2VideoSerializer
-    model_class = Form2Video
-
-class Form3VideoListView(BaseVideoListView):
-    serializer_class = Form3VideoSerializer
-    model_class = Form3Video
-
-class Form4VideoListView(BaseVideoListView):
-    serializer_class = Form4VideoSerializer
-    model_class = Form4Video
-
+# Category views
 class CategoriesView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
     def get(self, request, *args, **kwargs):
-        categories = Categories.objects.all()
+        categories = Category.objects.all()
         serializer = CategoriesSerializer(categories, many=True)
         return Response(serializer.data)
 
