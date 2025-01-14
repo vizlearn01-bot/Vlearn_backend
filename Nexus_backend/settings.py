87041008fb1ca5f8242a9639fac1f63e332915e1
Griffin <omondigriffin4@gmail.com>
2025-01-14 14:30:56 +0300

    updates the allowed hosts

--- Nexus_backend/settings.py
+++ Nexus_backend/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "localhost",
     '127.0.0.1',
-    'https://nexus-backend-k66x.onrender.com',
+    'https://vlearn-backend-254w.onrender.com',
     'api.vizlearn.co'
 ]
 # Application definition
