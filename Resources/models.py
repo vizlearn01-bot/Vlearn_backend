eebffd36840e5d4af3c7b4f1ff496ee9168596c3
Griffin <omondigriffin4@gmail.com>
2025-01-22 00:32:21 +0300

    changes the videocourse model field

--- Resources/models.py
+++ Resources/models.py
@@ -54,8 +54,8 @@ class VideoCourse(models.Model):
     instructor = models.CharField(max_length=255)
     rating = models.DecimalField(max_digits=3, decimal_places=1)
     video_link = models.URLField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
