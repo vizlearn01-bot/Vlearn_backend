4ea6f7d90619e7a16aa6f0063a10ea1525595b88
Griffin <omondigriffin4@gmail.com>
2025-02-17 19:35:16 +0300

    changes to the database

--- Nexus_backend/settings.py
+++ Nexus_backend/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 from dotenv import load_dotenv
-load_dotenv()  # Load environment variables from a .env file if available
+load_dotenv()  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,19 +103,12 @@ WSGI_APPLICATION = 'Nexus_backend.wsgi.application'
 #     }
 # }
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
+    "default": dj_database_url.config(
+        default=os.getenv('DATABASE_URL'), conn_max_age=600
+    )
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'vizlearn_science',
-#         'USER': 'vizlearn_user',
-#         'PASSWORD': '@Vizlearn4',
-#         # 'HOST': 'Local Host',  # Use localhost for cPanel
-#         # 'PORT': '5432',  # Default PostgreSQL port
-#     }
-# }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
