8940e1446ff1a3097059ed01b8f6d32fdf33efc6
Griffin <omondigriffin4@gmail.com>
2025-02-25 05:37:56 +0300

    creates url endpoints for user login and registrations

--- Resources/urls.py
+++ Resources/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path
-from .views import (
-    VideoUploadView,
-    VideoListView,
-    CategoriesView,
-    VideoCourseView,
-    CourseDetailView,
-    Home
+from .views import (VideoUploadView, VideoListView, CategoriesView, VideoCourseView, CourseDetailView, Home, RegisterView, LoginView, UserProfileView
 )
 
 urlpatterns = [
@@ -20,4 +14,8 @@ urlpatterns = [
 
     path('course_videos/', VideoCourseView.as_view(), name='video_courses'),
     path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail')
+
+    path('register/', RegisterView.as_view(), name='register')
+    path('login/', LoginView.as_view(), name= 'login')
+    path('profile/', UserProfileView.as_view(), name='profile')
 ]
