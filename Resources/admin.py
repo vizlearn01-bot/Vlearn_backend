dcec607ed650d6d8c88b9d1f37a2f78503ae2fff
Griffin <omondigriffin4@gmail.com>
2025-03-19 17:54:25 +0300

    creates a model to track user video interactions

--- Resources/admin.py
+++ Resources/admin.py
@@ -4,6 +4,8 @@ from .models import Category, ExperimentVideo, User, UserProfile
 admin.site.register(Category)
 admin.site.register(User)
 admin.site.register(UserProfile)
+# admin.site.register(ExperimentVideo)
+
 @admin.register(ExperimentVideo)
 class VideoCourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'instructor', 'difficulty', 'rating', 'created_at')
