8320f73f20346d38af0cb2921f2968d91858ce18
Griffin <omondigriffin4@gmail.com>
2025-04-10 15:29:36 +0300

    creates a model that should generate an access token for mpesa api

--- Resources/models.py
+++ Resources/models.py
@@ -140,3 +140,13 @@ class UserSubscription(models.Model):
 
     def __str__(self):
         return f"{self.user.email} - {self.plan.name}"
+
+class AccessToken(models.Model):
+	token = models.CharField(max_length=30)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		get_latest_by = 'created_at'
+
+	def __str__(self):
+		return self.token
