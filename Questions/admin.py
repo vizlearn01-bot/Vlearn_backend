a96815517cbab609bf542ec960830ab7b12b5524
Griffin <omondigriffin4@gmail.com>
2025-07-08 17:45:56 +0300

    Refactor: changes the admin layout for questions and the answers by introducing inline stacking

--- Questions/admin.py
+++ Questions/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from .models import Question, Quiz, QuestionAttempt, StudentAnswer, Answer
 
 # Register your models here.
-admin.site.register(Quiz)
-admin.site.register(Question)
+
+
+class AnswerInLine(admin.StackedInline):
+    model = Answer
+    extra = 4
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInLine]
+    
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    show_change_link = True
+    can_delete = False
+    extra = 0
+    
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(QuestionAttempt)
 admin.site.register(StudentAnswer)
-admin.site.register(Answer)
