39b1bb0d37261f6532ace53f64347f4cb3da4437
Griffin <omondigriffin4@gmail.com>
2025-07-31 09:29:52 +0300

    Refactor: creates a new app to handle the admin panel

--- Nexus_backend/urls.py
+++ Nexus_backend/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("api/subscriptions/", include("subscriptions.api.urls")),
     path("api/billing-and-payments/", include("billing_payment.api.urls")),
     path("questions/", include("Questions.urls")),
+    path("admin/", include("Admin.urls")),
     path("", include("Resources.urls")),
 ]
 
